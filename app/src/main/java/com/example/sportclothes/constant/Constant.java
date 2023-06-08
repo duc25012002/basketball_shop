3ae0fb567db00cbce818e13f875e5705424ec38a
hoangDuc <82940464+duc25012002@users.noreply.github.com>
2023-06-08 17:13:38 +0700

    ok

---
+++ app/src/main/java/com/example/sportclothes/constant/Constant.java
@@ -0,0 +1,23 @@
+package com.example.sportclothes.constant;
+
+public interface Constant {
+
+    String GENERIC_ERROR = "Fatal error! Try again!";
+
+    String PAGE_FACEBOOK = "https://www.facebook.com/";
+    String LINK_FACEBOOK = "https://www.facebook.com/";
+    String LINK_YOUTUBE = "";
+    String PHONE_NUMBER = "+84793157708";
+    String GMAIL = "ductrieuhoang@gmail.com";
+    String SKYPE_ID = "";
+    String ZALO_LINK = "";
+
+    String FIREBASE_URL = "https://serene-flare-367716-default-rtdb.firebaseio.com/";
+
+    String CURRENCY = ".000.000 VNĐ";
+    int TYPE_PAYMENT_CASH = 1;
+    String PAYMENT_METHOD_CASH = "Tiền mặt";
+
+    // Key Intent
+    String KEY_INTENT_CLOTH_OBJECT = "sport_clothes_project";
+}
